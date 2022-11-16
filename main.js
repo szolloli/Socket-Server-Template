6c97d76db09fd99ebd4bc4404e8919f4964086f0
szolloli <58864944+szolloli@users.noreply.github.com>
2022-11-16 23:23:53 +0100

    Add drawing canvas

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+app.set("view engine", "pug");
 
 app.use(express.static("public"));
 // require("dotenv").config();
@@ -17,7 +18,9 @@ const wss =
     : new WebSocket.Server({ port: 5001 });
 
 server.listen(port);
-console.log(`Server started on port ${process.env.PORT} in stage ${process.env.NODE_ENV}`);
+console.log(
+  `Server started on port ${process.env.PORT} in stage ${process.env.NODE_ENV}`,
+);
 
 wss.on("connection", function (ws, req) {
   console.log("Connection Opened");
@@ -32,14 +35,14 @@ wss.on("connection", function (ws, req) {
     if (isJSON(data)) {
       const currData = JSON.parse(data);
       broadcast(ws, currData, false);
-    } else if(typeof currData === 'string') {
-      if(currData === 'pong') {
-        console.log('keepAlive');
+    } else if (typeof currData === "string") {
+      if (currData === "pong") {
+        console.log("keepAlive");
         return;
       }
       broadcast(ws, currData, false);
     } else {
-      console.error('malformed message', data);
+      console.error("malformed message", data);
     }
   });
 
@@ -82,17 +85,246 @@ const isJSON = (message) => {
 /**
  * Sends a ping message to all connected clients every 50 seconds
  */
- const keepServerAlive = () => {
+const keepServerAlive = () => {
   keepAliveId = setInterval(() => {
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send('ping');
+        client.send("ping");
       }
     });
   }, 50000);
 };
 
+//////////////////////////////////
+var canvas,
+  ctx,
+  flag = false,
+  prevX = 0,
+  currX = 0,
+  prevY = 0,
+  currY = 0,
+  dot_flag = false;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+var x = "black",
+  y = 2;
+
+function init() {
+  console.log("kokotpicauholica");
+  canvas = document.getElementById("can");
+  canvas.width = document.body.clientWidth - 4;
+  canvas.height = document.body.clientHeight - 4;
+  ctx = canvas.getContext("2d");
+  w = canvas.width;
+  h = canvas.height;
+
+  canvas.addEventListener(
+    "mousemove",
+    function (e) {
+      console.log("mousemove");
+      findxy("move", e);
+    },
+    false,
+  );
+  canvas.addEventListener(
+    "mousedown",
+    function (e) {
+      console.log("mousedown");
+      findxy("down", e);
+    },
+    false,
+  );
+  canvas.addEventListener(
+    "mouseup",
+    function (e) {
+      console.log("mouseup");
+      findxy("up", e);
+    },
+    false,
+  );
+  canvas.addEventListener(
+    "mouseout",
+    function (e) {
+      console.log("mouseout");
+      findxy("out", e);
+    },
+    false,
+  );
+
+  canvas.addEventListener(
+    "touchstart",
+    function (e) {
+      console.log("touchsart");
+      findxy("down", e);
+
+      foo();
+    },
+    false,
+  );
+  canvas.addEventListener(
+    "touchmove",
+    function (e) {
+      console.log("touchmove", flag, e);
+      let touch = e.touches[0];
+      const mouseEvent = new Event("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY,
+      });
+      findxy("move", {
+        clientX: touch.clientX,
+        clientY: touch.clientY,
+      });
+      //canvas.dispatchEvent(mouseEvent);
+    },
+    false,
+  );
+}
+
+//////////////////////////////////
+
+const html = `<html>
+<script type="text/javascript">
+var canvas, ctx, flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+
+var x = "black",
+    y = 2;
+
+
+
+function init() {
+    let ws = new WebSocket('wss://td-rwen-drawing.herokuapp.com/:443');
+    ws.addEventListener('open', () => {
+        console.log('opened');
+    });
+
+    ws.addEventListener('message', () => {
+        console.log('message');
+    });
+
+    ws.addEventListener('open', () => {
+        console.log('error');
+    });
+
+    ws.addEventListener('close', () => {
+        console.log('closed');
+    });
+
+
+
+    canvas = document.getElementById('can');
+    canvas.width = document.body.clientWidth-4;
+    canvas.height = document.body.clientHeight-4; 
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
+
+    canvas.addEventListener("mousemove", function (e) {
+        console.log('mousemove')
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        console.log('mousedown')
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        console.log('mouseup')
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        console.log('mouseout')
+        findxy('out', e)
+    }, false);
+
+    canvas.addEventListener("touchstart", function (e) {
+        console.log('touchsart')
+        findxy('down', e)
+
+        foo()
+    }, false);
+    canvas.addEventListener("touchmove", function (e) {
+        console.log('touchmove', flag, e)
+        let touch = e.touches[0];
+        console.log('relative width: ', touch.clientX/w);
+        console.log('relative height: ',touch.clientY/h);
+        ws.send(JSON.stringify({"tx": touch.clientX/w, "ty": touch.clientY/h}))
+        const mouseEvent = new Event("mousemove", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        findxy('move', {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        })
+        //canvas.dispatchEvent(mouseEvent);
+    }, false);
+}
+
+function draw() {
+    console.log('drawing', prevX, prevY, currX, currY);
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function erase() {
+    ctx.clearRect(0, 0, w, h);
+    document.getElementById("canvasimg").style.display = "none";
+}
+
+function findxy(res, e) {
+    console.log('findingxy');
+    if (res == 'down') {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
+    }
+    if (res == 'up' || res == "out") {
+        flag = false;
+    }
+    if (res == 'move') {
+        console.log('findinf moving');
+        if (flag) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    }
+}
+</script>
+<style>
+    html,body {
+        padding: 0;
+        margin: 0;
+        overflow: hidden;
+    }
+</style>
+<body onload="init()">
+    <canvas id="can" style="border:2px solid;"></canvas>
+    <input type="button" value="clear" id="clr" size="23" onclick="erase()" style="position:absolute;top:55%;left:15%;">
+</body>
+</html>`;
+
+app.get("/", (req, res) => {
+  res.send(html);
 });
